769d93e365eb776390b37ccf8ba52188f4062c1d
ConzZah <69615452+ConzZah@users.noreply.github.com>
2026-06-15 06:28:52 +0200

    Create sat.sh

---
+++ sat.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+### /// setup-alpine-termux // ConzZah /// ###
+
+echo '== setup-alpine-termux // (c) ConzZah 2026 =='
+
+## set path to alpine prootfs
+alprootfs="$PREFIX/var/lib/proot-distro/containers/alpine/rootfs/"
+
+## if $alprootfs exists already, stop here.
+[ -d "$alprootfs" ] && printf '\n%s\n' "--> ALPINE-ROOTFS EXISTS ALREADY." && exit 0
+
+## update termux packages before continuing
+yes| pkg up || exit 1
+
+## if proot-distro is not installed, do so now
+! command -v proot-distro >/dev/null && \
+apt update && apt install -y proot-distro
+
+## install alpine
+proot-distro install alpine
+
+## set $alp_alias
+alp_alias='alias alp="proot-distro login alpine"'
+
+## if $alp_alias couldn't be found in .profile, add it
+touch "$HOME/.profile"
+! grep -q "$alp_alias" "$HOME/.profile" && \
+printf '%s\n' "$alp_alias" >> "$HOME/.profile"
+
+## add .profile to $alprootfs to isolate termux binaries
+[ ! -f  ${alprootfs}/root/.profile ] && \
+printf '%s\n' '# exclude termux bins, so we dont use them when building
+PATH="$(echo "$PATH"| sed "s#:/data/data/com.termux/files/usr/bin##g")"' > ${alprootfs}/root/.profile
+
+## change login shell to bash and install basic packages
+proot-distro login alpine -- apk add build-base curl git bash nano shadow fastfetch
+proot-distro login alpine -- chsh -s /bin/bash root >/dev/null
+
+## login
+proot-distro login alpine
